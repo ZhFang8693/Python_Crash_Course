e0dd6a71bb3985e23114ac18ed5aa38f324fce8d
ZhFang <zhangfang8693@outlook.com>
2022-04-19 11:17:27 +0800

    Update chap8-3返回值.py
    add some notice

--- chap8-3返回值.py
+++ chap8-3返回值.py
@@ -7,7 +7,7 @@ my_name = full_names('zhang', 'fang')
 print (my_name)
 
 # 默认值作为"可选值"的操作
-# 指定为默认值的,要在后面,不能在前面
+# 指定为默认值的,要在后面,不能在前面,且一次只能指定一个默认值
 def ful_name(first_name,last_name,middle_name=''):
     full_name_1 = first_name + ' ' + middle_name + ' ' + last_name
     return full_name_1.title()
